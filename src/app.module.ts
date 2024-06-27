b967764e17748b076b3b641654fac4fa96f328d3
Alexandre Tondello <alextondello@gmail.com>
2024-06-26 21:26:11 -0300

    Remove unused files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,8 @@
 import { Module } from '@nestjs/common';
 
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { EligibilityModule } from './eligibility/eligibility.module';
 
 @Module({
   imports: [EligibilityModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
